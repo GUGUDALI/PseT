dfbfdd69cfdc15e69feea65190b350dcb0fe223c
Selimcan "GUGUDALI" YILDIZ <55906355+GUGUDALI@users.noreply.github.com>
2021-04-25 18:10:47 +0300

    Add files via upload

---
+++ PSet/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace PSet
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer Created Code
+
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Apply = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Apply
+            // 
+            this.Apply.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Apply.ForeColor = System.Drawing.SystemColors.Control;
+            this.Apply.Location = new System.Drawing.Point(264, 14);
+            this.Apply.Name = "Apply";
+            this.Apply.Size = new System.Drawing.Size(63, 23);
+            this.Apply.TabIndex = 0;
+            this.Apply.Text = "Apply";
+            this.Apply.UseVisualStyleBackColor = false;
+            this.Apply.Click += new System.EventHandler(this.Apply_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 44);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.ForeColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.ShowCellErrors = false;
+            this.dataGridView1.ShowCellToolTips = false;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.ShowRowErrors = false;
+            this.dataGridView1.Size = new System.Drawing.Size(416, 554);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.Control;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Realtime",
+            "High",
+            "Above Normal",
+            "Normal",
+            "Below Normal",
+            "Low"});
+            this.comboBox1.Location = new System.Drawing.Point(120, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(138, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Refresh List";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(418, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 27);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "X";
+            this.label1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Label1_MouseClick);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Wide Latin", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(393, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 28);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "_";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(448, 610);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Apply);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pset";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Apply;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PSet/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PSet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Process[] processes;
+        ProcessPriorityClass Pclass;
+        bool Pboost;
+        int[] cores = new int[Environment.ProcessorCount];
+        int core;
+        int dtWidth = 0;
+        bool fmoving = false;
+        Point spoint = new Point(0, 0);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < cores.Length; i++)
+            {
+                if (i == 0)
+                {
+                    cores[i] = 1;
+                }
+                else
+                {
+                    cores[i] = cores[i - 1] + cores[i - 1];
+                }
+            }
+            dataGridView1.Columns.Add("Pid", "PID");
+            dataGridView1.Columns.Add("PName", "Name");
+            dataGridView1.Columns.Add("PStat", "Priority");
+            dataGridView1.Columns.Add("PBoost", "Boost");
+            comboBox1.SelectedText = "Low";
+            Refresh_Table();
+        }
+        private void Apply_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedItem)
+            {
+                case "Realtime":
+                    Pclass = ProcessPriorityClass.RealTime;
+                    Pboost = true;
+                    break;
+                case "High":
+                    Pclass = ProcessPriorityClass.High;
+                    Pboost = true;
+                    break;
+                case "Above Normal":
+                    Pclass = ProcessPriorityClass.AboveNormal;
+                    Pboost = true;
+                    break;
+                case "Normal":
+                    Pclass = ProcessPriorityClass.Normal;
+                    Pboost = false;
+                    break;
+                case "Below Normal":
+                    Pclass = ProcessPriorityClass.BelowNormal;
+                    Pboost = false;
+                    break;
+                case "Low":
+                    Pclass = ProcessPriorityClass.Idle;
+                    Pboost = false;
+                    break;
+                default:
+                    Pclass = ProcessPriorityClass.Idle;
+                    Pboost = false;
+                    break;
+            }
+            processes = Process.GetProcesses();
+            foreach (DataGridViewRow dataGridViewRow in dataGridView1.SelectedRows)
+            {
+                foreach (var proc in processes)
+                {
+                    if (proc.Id == (int)dataGridViewRow.Cells["Pid"].Value)
+                    {
+                        try
+                        {
+                            core = 0;
+                            if (Environment.ProcessorCount >= 12)
+                            {
+                                if (proc.ProcessName == "svchost" || proc.ProcessName == Process.GetCurrentProcess().ProcessName)
+                                {
+                                    proc.PriorityClass = Pclass;
+                                }
+                                else
+                                {
+                                    proc.PriorityClass = Pclass;
+                                    proc.PriorityBoostEnabled = Pboost;
+                                    if (Pclass == ProcessPriorityClass.Idle)
+                                    {
+                                        for (int a = 0; a < cores.Length; a++)
+                                        {
+                                            if (a == 0 || a == 1 || a == (cores.Length / 2) || a == cores.Length / 2 + 1)
+                                            {
+                                                
+                                            }
+                                            else
+                                            {
+                                                core += cores[a];
+                                            }
+                                        }
+                                        proc.ProcessorAffinity = (IntPtr)core;
+                                    }
+                                    else if (Pclass == ProcessPriorityClass.BelowNormal)
+                                    {
+                                        for (int a = 0; a < cores.Length; a++)
+                                        {
+                                            if (a == 0 || a == 1 || a == (cores.Length / 2) || a == cores.Length / 2 + 1)
+                                            {
+
+                                            }
+                                            else
+                                            {
+                                                core += cores[a];
+                                            }
+                                        }
+                                        proc.ProcessorAffinity = (IntPtr)core;
+                                    }
+                                    else if (Pclass == ProcessPriorityClass.Normal)
+                                    {
+                                        for (int a = 0; a < cores.Length; a++)
+                                        {
+                                            if (a == 0 || a == 1 || a == (cores.Length / 2) || a == cores.Length / 2 + 1)
+                                            {
+
+                                            }
+                                            else
+                                            {
+                                                core += cores[a];
+                                            }
+                                        }
+                                        proc.ProcessorAffinity = (IntPtr)core;
+                                    }
+                                    else if (Pclass == ProcessPriorityClass.AboveNormal)
+                                    {
+                                        for (int a = 0; a < cores.Length; a++)
+                                        {
+                                            if (a == 0 || a == (cores.Length / 2) - 1)
+                                            {
+
+                                            }
+                                            else
+                                            {
+                                                core += cores[a];
+                                            }
+                                        }
+                                        proc.ProcessorAffinity = (IntPtr)core;
+                                    }
+                                    else if (Pclass == ProcessPriorityClass.High)
+                                    {
+                                        for (int a = 0; a < cores.Length; a++)
+                                        {
+                                            if (a == 0 || a == (cores.Length / 2) - 1)
+                                            {
+
+                                            }
+                                            else
+                                            {
+                                                core += cores[a];
+                                            }
+                                        }
+                                        proc.ProcessorAffinity = (IntPtr)core;
+                                    }
+                                    else if (Pclass == ProcessPriorityClass.RealTime)
+                                    {
+                                        for (int a = 0; a < cores.Length; a++)
+                                        {
+                                            core += cores[a];
+                                        }
+                                        proc.ProcessorAffinity = (IntPtr)core;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                if (proc.ProcessName == "svchost" || proc.ProcessName == Process.GetCurrentProcess().ProcessName)
+                                {
+                                    
+                                }
+                                else
+                                {
+                                    proc.PriorityClass = Pclass;
+                                }
+                            }
+                        }
+                        catch { }
+                    }
+                }
+            }
+            Refresh_Table();
+        }
+        private void Refresh_Table()
+        {
+            dtWidth = 0;
+            dataGridView1.Rows.Clear();
+            processes = Process.GetProcesses();
+            foreach (var proc in processes)
+            {
+                try
+                {
+                    dataGridView1.Rows.Add(proc.Id, proc.ProcessName + ".exe", proc.PriorityClass, proc.PriorityBoostEnabled);
+                }
+                catch { }
+            }
+            dataGridView1.Sort(dataGridView1.Columns[1], ListSortDirection.Ascending);
+            foreach (DataGridViewColumn clmn in dataGridView1.Columns)
+            {
+                dtWidth += clmn.Width;
+            }
+            dataGridView1.Width = dtWidth + 20;
+            this.Width = dataGridView1.Width + 25;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Refresh_Table();
+        }
+
+        private void Label1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            fmoving = true;
+            spoint = new Point(e.X, e.Y);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            fmoving = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (fmoving)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this.spoint.X, p.Y - this.spoint.Y);
+            }
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
